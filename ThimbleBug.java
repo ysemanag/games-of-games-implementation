131974e6d1e8b25e107e85e4899493f11a427aec
Azurite <ehshi16@gmail.com>
2022-12-04 00:41:20 -0500

    triple thimble threat
    all three versions of the thimble class

--- ThimbleBug.java
+++ ThimbleBug.java
@@ -8,7 +8,7 @@
 
 import java.util.*;
 
-public class Thimble
+public class ThimbleBug
 {
     // instance variables - replace the example below with your own
     private int rounds = 0;
@@ -20,39 +20,39 @@ public class Thimble
     /**
      * Constructor for objects of class Thimble
      */
-    public Thimble()
+    public ThimbleBug()
     {
     }
 
     public boolean thimbleMatch() {
-
         rounds = fetch.getBestOf();
-        while(hiddenPoints < rounds / 2 + 1 && foundPoints < rounds / 2 + 1) {
+        while(hiddenPoints <= rounds / 2 + 1 && foundPoints <= rounds / 2 + 1) {
             if(thimbleRound()) {
-                System.out.println("Thimble found! Player wins a round."); // < i wrote this text
+                System.out.println("Thimble found! Player wins a round.");
                 foundPoints++;
             } else {
-                System.out.println("Thimble not found! Computer wins a round."); // < i wrote this text
+                System.out.println("Thimble not found! Computer wins a round.");
                 hiddenPoints++;
             }
 
             System.out.println("[TEST MODE: CURRENT SCORE: Computer " + hiddenPoints + " - " + foundPoints + " Player]");
         }
-        if(foundPoints < hiddenPoints) {
-            System.out.println("Computer is *winner*");
+        if(foundPoints > hiddenPoints) {
+            System.out.println("The computer is the winner!");
         } else {
-            System.out.println("Player is the *winner*");
+            System.out.println("The player is the winner!");
         }
         return foundPoints > hiddenPoints; // true = player 1, false = player 2;
     }
 
     public boolean thimbleRound() {
-        System.out.println("The computer now hides the thimble."); // < i wrote this text
-        String hidden = "l";
+        System.out.println("The computer now hides the thimble.");
+        String hidden = "i";
         if(r.nextBoolean()) {
             hidden = "r";
         }
-        System.out.println("The player now opens a hand."); // < i wrote this text
+        System.out.printf("[TEST MODE: Computer's thimble hand is: %s]\n", hidden);
+        System.out.println("The player now opens a hand.");
         String find = fetch.pickBetween("left", "l", "right", "r");
         return hidden.equals(find);
     }
